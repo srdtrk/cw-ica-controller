863b4335743ed1d9aced473b1649a740cd1c154e
srdtrk <srdtrk@hotmail.com>
2023-06-15 19:24:16 +0200

    feat: ran 'cargo generate --git https://github.com/CosmWasm/cw-template.git --name cosmwasm-ica-controller -d minimal=true'

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use cosmwasm_ica_controller::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
