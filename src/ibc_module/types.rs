1ee856d777058738215fc2bc67534a710bcb9010
srdtrk <srdtrk@hotmail.com>
2023-06-16 15:55:37 +0200

    imp: new types added

--- src/ibc_module/types.rs
+++ src/ibc_module/types.rs
@@ -1,2 +1,5 @@
 // Version defines the current version for interchain accounts module
 const ICA_VERSION: &str = "ics27-1";
+
+// HOST_PORT_ID is the default port id that the interchain accounts host submodule binds to
+const HOST_PORT_ID: &str = "icahost";
