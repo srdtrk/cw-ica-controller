df6feaac8b23cb13217f6514f03f9fe9034d42fe
srdtrk <srdtrk@hotmail.com>
2023-06-17 02:41:55 +0200

    feat: added ContractState

--- src/state.rs
+++ src/state.rs
@@ -1,17 +1,25 @@
-use cosmwasm_std::IbcChannel;
+use cosmwasm_std::{Addr, IbcChannel};
 use cw_storage_plus::Item;
 use serde::{Deserialize, Serialize};
 
 pub const CHANNEL_STATE: Item<ContractChannelState> = Item::new("ica_channel");
 
-/// ContractState is the state of the IBC application
+/// ContractState is the state of the IBC application.
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct ContractState {
+    pub admin: Addr,
+    pub ica_address: Option<String>,
+}
+
+/// ContractChannelState is the state of the IBC application's channel.
+/// This application only supports one channel.
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ContractChannelState {
     pub channel: IbcChannel,
     pub channel_state: ChannelState,
 }
 
-/// ChannelState is the state of the IBC channel
+/// ChannelState is the state of the IBC channel.
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum ChannelState {
     Uninitialized,
