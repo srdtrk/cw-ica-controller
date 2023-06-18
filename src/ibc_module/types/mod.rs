59aee8f3baba810768f2e9eef59abab5a58ee113
srdtrk <srdtrk@hotmail.com>
2023-06-18 12:25:53 +0200

    feat: added packet.rs

--- src/ibc_module/types/mod.rs
+++ src/ibc_module/types/mod.rs
@@ -1,2 +1,3 @@
 pub mod keys;
 pub mod metadata;
+pub mod packet;
---
+++ src/ibc_module/types/packet.rs
@@ -0,0 +1,22 @@
+use cosmwasm_schema::cw_serde;
+
+/// InterchainAccountPacketData is comprised of a raw transaction, type of transaction and optional memo field.
+#[cw_serde]
+pub struct InterchainAccountPacketData {
+    /// Type defines a classification of message issued from a controller
+    /// chain to its associated interchain accounts host.
+    ///
+    /// There are two types of messages:
+    /// * `0 = Unspecified`: Default zero value enumeration. (Returns an error in host).
+    /// * `1 = ExecuteTx`: Execute a transaction on an interchain accounts host chain.
+    ///
+    /// `r#type` is used to avoid the reserved keyword `type`.
+    #[serde(rename = "type")]
+    pub r#type: u32,
+    /// Data is the raw transaction data that will be sent to the interchain accounts host.
+    pub data: Vec<u8>,
+    /// Memo is an optional field that can be used to attach a memo to a transaction.
+    /// It is also caught by some ibc middleware to perform additional actions.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub memo: Option<String>,
+}
