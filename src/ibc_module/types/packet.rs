75debd9df6059a7be881f407c8defbb6aa5a094a
srdtrk <srdtrk@hotmail.com>
2023-06-18 15:27:40 +0200

    imp: added more helpers to packet

--- src/ibc_module/types/packet.rs
+++ src/ibc_module/types/packet.rs
@@ -1,6 +1,10 @@
 use cosmwasm_schema::cw_serde;
+use cosmwasm_std::to_vec;
+
+use crate::types::ContractError;
 
 /// InterchainAccountPacketData is comprised of a raw transaction, type of transaction and optional memo field.
+/// Currently, the host only supports serialized [`IcaCosmosTx`](ica_cosmos_tx::IcaCosmosTx) messages as raw transactions.
 #[cw_serde]
 pub struct InterchainAccountPacketData {
     /// Type defines a classification of message issued from a controller
@@ -14,6 +18,7 @@ pub struct InterchainAccountPacketData {
     #[serde(rename = "type")]
     pub r#type: u32,
     /// Data is the raw transaction data that will be sent to the interchain accounts host.
+    /// Currently, the host only supports serialized [`IcaCosmosTx`](ica_cosmos_tx::IcaCosmosTx) messages.
     pub data: Vec<u8>,
     /// Memo is an optional field that can be used to attach a memo to a transaction.
     /// It is also caught by some ibc middleware to perform additional actions.
@@ -30,16 +35,55 @@ impl InterchainAccountPacketData {
             memo,
         }
     }
+
+    /// Creates a new InterchainAccountPacketData from an [`IcaCosmosTx`](ica_cosmos_tx::IcaCosmosTx)
+    pub fn from_cosmos_tx(
+        cosmos_tx: ica_cosmos_tx::IcaCosmosTx,
+        memo: Option<String>,
+    ) -> Result<Self, ContractError> {
+        Ok(Self::new(to_vec(&cosmos_tx)?, memo))
+    }
+
+    /// Creates a new InterchainAccountPacketData from a list of JSON strings
+    ///
+    /// The messages must be serialized as JSON strings in the format expected by the ICA host.
+    /// The following is an example of a serialized [`IcaCosmosTx`](ica_cosmos_tx::IcaCosmosTx) with one legacy gov proposal message:
+    ///
+    /// ## Format
+    ///
+    /// ```json
+    /// {
+    ///   "messages": [
+    ///     {
+    ///       "@type": "/cosmos.gov.v1beta1.MsgSubmitProposal",
+    ///       "content": {
+    ///         "@type": "/cosmos.gov.v1beta1.TextProposal",
+    ///         "title": "IBC Gov Proposal",
+    ///         "description": "tokens for all!"
+    ///       },
+    ///       "initial_deposit": [{ "denom": "stake", "amount": "5000" }],
+    ///       "proposer": "cosmos1k4epd6js8aa7fk4e5l7u6dwttxfarwu6yald9hlyckngv59syuyqnlqvk8"
+    ///     }
+    ///   ]
+    /// }
+    /// ```
+    ///
+    /// In this example, the proposer must be the ICA controller's address.
+    pub fn from_strings(
+        messages: Vec<impl Into<String>>,
+        memo: Option<String>,
+    ) -> Result<Self, ContractError> {
+        let cosmos_tx = ica_cosmos_tx::IcaCosmosTx::from_strings(messages)?;
+        Self::from_cosmos_tx(cosmos_tx, memo)
+    }
 }
 
 pub mod ica_cosmos_tx {
-    use crate::types::ContractError;
-
     use super::*;
 
     /// IcaCosmosTx is a list of Cosmos messages that is sent to the ICA host.
     ///
-    /// ## Example
+    /// ## Format
     ///
     /// The messages must be serialized as JSON strings in the format expected by the ICA host.
     /// The following is an example of a serialized IcaCosmosTx with one legacy gov proposal message:
