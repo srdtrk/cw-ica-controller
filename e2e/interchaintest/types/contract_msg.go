48b989faf39003fd7a0ad70281d927a4da7bf344
srdtrk <srdtrk@hotmail.com>
2023-06-19 19:04:19 +0200

    feat(e2e): started implementing message helpers

--- e2e/interchaintest/types/contract_msg.go
+++ e2e/interchaintest/types/contract_msg.go
@@ -1,2 +1,42 @@
 package types
 
+import (
+	"encoding/base64"
+	"encoding/json"
+
+	codec "github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func NewSendCustomIcaMessagesMsg(cdc codec.BinaryCodec, msgs []sdk.Msg, memo *string, timeout *uint64) ([]byte, error) {
+	type SendCustomIcaMessagesMsg struct {
+		Messages       []string `json:"messages"`
+		PacketMemo     *string  `json:"packet_memo,omitempty"`
+		TimeoutSeconds *uint64  `json:"timeout_seconds,omitempty"`
+	}
+
+	type SendCustomIcaMessagesMsgWrapper struct {
+		SendCustomIcaMessagesMsg SendCustomIcaMessagesMsg `json:"send_custom_ica_messages"`
+	}
+
+	messages := make([]string, len(msgs))
+
+	for _, msg := range msgs {
+		bz, err := cdc.(*codec.ProtoCodec).MarshalJSON(msg)
+		if err != nil {
+			return nil, err
+		}
+		b64 := base64.StdEncoding.EncodeToString(bz)
+		messages = append(messages, b64)
+	}
+
+	msg := SendCustomIcaMessagesMsgWrapper{
+		SendCustomIcaMessagesMsg: SendCustomIcaMessagesMsg{
+			Messages:       messages,
+			PacketMemo:     memo,
+			TimeoutSeconds: timeout,
+		},
+	}
+
+	return json.Marshal(msg)
+}
