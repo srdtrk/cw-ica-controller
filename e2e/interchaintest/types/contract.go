0211d14363df8f08661049e973d2c1a246b30374
srdtrk <srdtrk@hotmail.com>
2023-06-19 19:04:36 +0200

    style(e2e): ran gofumpt

--- e2e/interchaintest/types/contract.go
+++ e2e/interchaintest/types/contract.go
@@ -2,20 +2,20 @@ package types
 
 // ContractState is used to represent its state in Contract's storage
 type ContractState struct {
-	Admin string `json:"admin"`
+	Admin   string          `json:"admin"`
 	IcaInfo ContractIcaInfo `json:"ica_info"`
 }
 
 // ContractIcaInfo is used to represent the ICA info in the contract's state
 type ContractIcaInfo struct {
 	IcaAddress string `json:"ica_address"`
-	ChannelID string `json:"channel_id"`
+	ChannelID  string `json:"channel_id"`
 }
 
 // ContractChannelState is used to represent the channel state in the contract's storage
 type ContractChannelState struct {
-	Channel CwIbcChannel `json:"channel"`
-	ChannelStatus string `json:"channel_status"`
+	Channel       CwIbcChannel `json:"channel"`
+	ChannelStatus string       `json:"channel_status"`
 }
 
 // IsOpen returns true if the channel is open
--- e2e/interchaintest/types/cosmwasm.go
+++ e2e/interchaintest/types/cosmwasm.go
@@ -2,16 +2,16 @@ package types
 
 // CwIbcEndpoint is the endpoint of a channel defined in CosmWasm
 type CwIbcEndpoint struct {
-	PortID string `json:"port_id"`
+	PortID    string `json:"port_id"`
 	ChannelID string `json:"channel_id"`
 }
 
 // CwIbcChannel is the channel defined in CosmWasm
 type CwIbcChannel struct {
-	Endpoint CwIbcEndpoint `json:"endpoint"`
+	Endpoint             CwIbcEndpoint `json:"endpoint"`
 	CounterpartyEndpoint CwIbcEndpoint `json:"counterparty_endpoint"`
 	// Order is either "ORDER_UNORDERED" or "ORDER_ORDERED"
-	Order string `json:"order"`
-	Version string `json:"version"`
+	Order        string `json:"order"`
+	Version      string `json:"version"`
 	ConnectionID string `json:"connection_id"`
 }
