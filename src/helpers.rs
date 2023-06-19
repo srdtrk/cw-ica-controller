60c665163909497f45a0f7a54763ab23563a534a
srdtrk <srdtrk@hotmail.com>
2023-06-19 13:10:15 +0200

    fix: fixed helper and added helper test

--- src/helpers.rs
+++ src/helpers.rs
@@ -13,6 +13,26 @@ pub const ICA_PLACEHOLDER: &str = "$ica_address";
 /// * `msg`         - Base64-encoded json message to deserialize and insert the ICA address into.
 /// * `ica_address` - ICA address to insert into the message.
 pub fn insert_ica_address(msg: Binary, ica_address: impl Into<String>) -> StdResult<String> {
-    let decoded: String = from_binary(&msg)?;
-    Ok(decoded.replace(ICA_PLACEHOLDER, &ica_address.into()))
+    let decoded: serde_json::Value = from_binary(&msg)?;
+    Ok(decoded
+        .to_string()
+        .replace(ICA_PLACEHOLDER, &ica_address.into()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insert_ica_address_works() {
+        let msg = Binary::from(br#"{"@type": "/cosmos.bank.v1beta1.MsgSend", "from_address": "$ica_address", "to_address": "cosmos15ulrf36d4wdtrtqzkgaan9ylwuhs7k7qz753uk", "amount": [{"denom": "stake", "amount": "5000"}]}"#);
+        let ica_address = "cosmos15ulrf36d4wdtrtqzkgaan9ylwuhs7k7qz753uk";
+
+        let expected: serde_json::Value = serde_json_wasm::from_str(
+            r#"{"@type": "/cosmos.bank.v1beta1.MsgSend", "from_address": "cosmos15ulrf36d4wdtrtqzkgaan9ylwuhs7k7qz753uk", "to_address": "cosmos15ulrf36d4wdtrtqzkgaan9ylwuhs7k7qz753uk", "amount": [{"denom": "stake", "amount": "5000"}]}"#,
+        ).unwrap();
+        let actual: serde_json::Value =
+            serde_json_wasm::from_str(&insert_ica_address(msg, ica_address).unwrap()).unwrap();
+        assert_eq!(actual, expected);
+    }
 }
