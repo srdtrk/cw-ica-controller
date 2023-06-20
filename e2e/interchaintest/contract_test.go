05f7a6727f9b729161d79f820e4ac9c5f8b88ed9
srdtrk <srdtrk@hotmail.com>
2023-06-20 12:49:42 +0200

    feat(e2e): starting to start chains

--- e2e/interchaintest/contract_test.go
+++ e2e/interchaintest/contract_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/strangelove-ventures/interchaintest/v7/ibc"
 	"github.com/strangelove-ventures/interchaintest/v7/relayer"
 	"github.com/strangelove-ventures/interchaintest/v7/testreporter"
+	"github.com/strangelove-ventures/interchaintest/v7/testutil"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
 )
@@ -27,19 +28,48 @@ func TestIcaControllerContract(t *testing.T) {
 
 	// Get both chains
 	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
-		{
-			Name: "icad",
-			ChainConfig: ibc.ChainConfig{
-				Images:                 []ibc.DockerImage{{Repository: "ghcr.io/srdtrk/wasmd", Version: "jsonica"}},
-				UsingNewGenesisCommand: true,
+
+		// -- REMOTE/LOCAL IMAGE EXAMPLE --
+  	{ChainConfig: ibc.ChainConfig{
+			Type: "cosmos",
+			Name: "wasmd",
+			ChainID: "wasmd-1",
+			Images: []ibc.DockerImage{
+					{
+							Repository: "ghcr.io/srdtrk/wasmd",   // FOR LOCAL IMAGE USE: Docker Image Name
+							Version: "jsonica",                   // FOR LOCAL IMAGE USE: Docker Image Tag  
+					},
 			},
+			Bin: "wasmd",
+			Bech32Prefix: "wasm",
+			Denom: "stake",
+			GasPrices: "0.00stake",
+			GasAdjustment: 1.3,
+			TrustingPeriod: "508h",
+			NoHostMount: false,
+			UsingNewGenesisCommand: true,
 		},
-		{
-			Name: "icad",
-			ChainConfig: ibc.ChainConfig{
-				Images:                 []ibc.DockerImage{{Repository: "ghcr.io/srdtrk/wasmd", Version: "jsonica"}},
-				UsingNewGenesisCommand: true,
+		},
+		// -- REMOTE/LOCAL IMAGE EXAMPLE --
+		{ChainConfig: ibc.ChainConfig{
+			Type: "cosmos",
+			Name: "wasmd",
+			ChainID: "wasmd-2",
+			Images: []ibc.DockerImage{
+					{
+							Repository: "ghcr.io/srdtrk/wasmd",   // FOR LOCAL IMAGE USE: Docker Image Name
+							Version: "jsonica",                   // FOR LOCAL IMAGE USE: Docker Image Tag  
+					},
 			},
+			Bin: "wasmd",
+			Bech32Prefix: "wasm",
+			Denom: "stake",
+			GasPrices: "0.00stake",
+			GasAdjustment: 1.3,
+			TrustingPeriod: "508h",
+			NoHostMount: false,
+			UsingNewGenesisCommand: true,
+		},
 		},
 	})
 
@@ -78,4 +108,46 @@ func TestIcaControllerContract(t *testing.T) {
 		NetworkID:        network,
 		SkipPathCreation: true,
 	}))
+
+	// Fund a user account on chain1 and chain2
+	// const userFunds = int64(10_000_000_000)
+	// users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, chain1, chain2)
+	// chain1User := users[0]
+	// chain2User := users[1]
+
+	// Generate a new IBC path
+	err = r.GeneratePath(ctx, eRep, chain1.Config().ChainID, chain2.Config().ChainID, pathName)
+	require.NoError(t, err)
+
+	// Create new clients
+	err = r.CreateClients(ctx, eRep, pathName, ibc.CreateClientOptions{TrustingPeriod: "330h"})
+	require.NoError(t, err)
+
+	err = testutil.WaitForBlocks(ctx, 2, chain1, chain2)
+	require.NoError(t, err)
+
+	// Create a new connection
+	err = r.CreateConnections(ctx, eRep, pathName)
+	require.NoError(t, err)
+
+	err = testutil.WaitForBlocks(ctx, 2, chain1, chain2)
+	require.NoError(t, err)
+
+	// Query for the newly created connection
+	connections, err := r.GetConnections(ctx, eRep, chain1.Config().ChainID)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(connections))
+
+	// Start the relayer and set the cleanup function.
+	err = r.StartRelayer(ctx, eRep, pathName)
+	require.NoError(t, err)
+
+	t.Cleanup(
+		func() {
+			err := r.StopRelayer(ctx, eRep)
+			if err != nil {
+				t.Logf("an error occurred while stopping the relayer: %s", err)
+			}
+		},
+	)
 }
