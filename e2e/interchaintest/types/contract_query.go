01e000dbb773fea796649f5c21de45f16c662e7d
srdtrk <srdtrk@hotmail.com>
2023-06-22 19:16:10 +0200

    imp(e2e): improved query helpers

--- e2e/interchaintest/types/contract_query.go
+++ e2e/interchaintest/types/contract_query.go
@@ -1,41 +1,10 @@
 package types
 
-import (
-	"encoding/json"
-)
-
-// NewGetChannelQueryMsg creates a new GetChannelQueryMsg.
-func NewGetChannelQueryMsg() []byte {
-	return []byte(`{"get_channel":{}}`)
+type QueryMsg struct {
+	GetChannel EmptyObject `json:"get_channel"`
+	GetContractState EmptyObject `json:"get_contract_state"`
+	GetCallbackCounter EmptyObject `json:"get_callback_counter"`
 }
 
-// NewGetContractStateQueryMsg creates a new GetContractStateQueryMsg.
-func NewGetContractStateQueryMsg() []byte {
-	return []byte(`{"get_contract_state":{}}`)
-}
+type EmptyObject struct {}
 
-// NewGetCallbackCounterQueryMsg creates a new GetCallbackCounterQueryMsg.
-func NewGetCallbackCounterQueryMsg() []byte {
-	return []byte(`{"get_callback_counter":{}}`)
-}
-
-// UnmarshalGetChannelResp unmarshals the response to a GetChannelQueryMsg.
-func UnmarshalGetChannelResp(bz []byte) (ContractChannelState, error) {
-	var msg ContractChannelState
-	err := json.Unmarshal(bz, &msg)
-	return msg, err
-}
-
-// UnmarshalGetContractStateResp unmarshals the response to a GetContractStateQueryMsg.
-func UnmarshalGetContractStateResp(bz []byte) (ContractState, error) {
-	var msg ContractState
-	err := json.Unmarshal(bz, &msg)
-	return msg, err
-}
-
-// UnmarshalGetCallbackCounterResp unmarshals the response to a GetCallbackCounterQueryMsg.
-func UnmarshalGetCallbackCounterResp(bz []byte) (ContractCallbackCounter, error) {
-	var msg ContractCallbackCounter
-	err := json.Unmarshal(bz, &msg)
-	return msg, err
-}
