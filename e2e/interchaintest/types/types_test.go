189ac8b830bdf4f6e8c677c5d56d5195d276c611
srdtrk <srdtrk@hotmail.com>
2023-06-23 16:58:59 +0200

    imp(e2e): added more types tests

--- e2e/interchaintest/types/types_test.go
+++ e2e/interchaintest/types/types_test.go
@@ -8,6 +8,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestInstantiateMsg(t *testing.T) {
+	t.Parallel()
+
+	msg := types.NewInstantiateMsg(nil)
+	require.Equal(t, `{}`, msg)
+
+	admin := "srdtrk"
+	msg = types.NewInstantiateMsg(&admin)
+	require.Equal(t, `{"admin":"srdtrk"}`, msg)
+}
+
+func TestExecuteMsgs(t *testing.T) {
+	t.Parallel()
+
+	sendPredefinedActionMsg := types.NewSendPredefinedActionMsg("srdtrk")
+	require.Equal(t, `{"send_predefined_action":{"to_address":"srdtrk"}}`, sendPredefinedActionMsg)
+
+	sendCustomIcaMessagesMsg := types.NewSendCustomIcaMessagesMsg(nil, nil, nil, nil)
+	require.Equal(t, `{"send_custom_ica_messages":{"messages":[]}}`, sendCustomIcaMessagesMsg)
+	memo := "test"
+	sendCustomIcaMessagesMsg = types.NewSendCustomIcaMessagesMsg(nil, nil, &memo, nil)
+	require.Equal(t, `{"send_custom_ica_messages":{"messages":[],"packet_memo":"test"}}`, sendCustomIcaMessagesMsg)
+	timeout := uint64(150)
+	sendCustomIcaMessagesMsg = types.NewSendCustomIcaMessagesMsg(nil, nil, nil, &timeout)
+	require.Equal(t, `{"send_custom_ica_messages":{"messages":[],"timeout_seconds":150}}`, sendCustomIcaMessagesMsg)
+}
+
 func TestQueries(t *testing.T) {
 	t.Parallel()
 
