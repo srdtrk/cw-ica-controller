7487bccafe47b8e26f19905c6a0c92d5293bd16d
srdtrk <srdtrk@hotmail.com>
2023-06-24 14:20:10 +0200

    refactor(e2e): suite works now

--- e2e/interchaintest/contract_test.go
+++ e2e/interchaintest/contract_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"testing"
 
+	mysuite "github.com/srdtrk/cw-ica-controller/interchaintest/v2/testsuite"
 	"github.com/srdtrk/cw-ica-controller/interchaintest/v2/types"
 
 	sdkmath "cosmossdk.io/math"
@@ -15,34 +16,33 @@ import (
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 
 	interchaintest "github.com/strangelove-ventures/interchaintest/v7"
-	"github.com/strangelove-ventures/interchaintest/v7/chain/cosmos"
 	"github.com/strangelove-ventures/interchaintest/v7/chain/cosmos/wasm"
 	"github.com/strangelove-ventures/interchaintest/v7/ibc"
-	"github.com/strangelove-ventures/interchaintest/v7/relayer"
-	"github.com/strangelove-ventures/interchaintest/v7/testreporter"
 	"github.com/strangelove-ventures/interchaintest/v7/testutil"
 
 	icatypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/types"
 	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 
-	"github.com/stretchr/testify/require"
-	"go.uber.org/zap/zaptest"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestIcaControllerContract(t *testing.T) {
+type ContractTestSuite struct {
+	mysuite.TestSuite
+}
+
+func TestWithContractTestSuite(t *testing.T) {
+	suite.Run(t, new(ContractTestSuite))
+}
+
+func (s *ContractTestSuite) TestIcaControllerContract() {
 	// Parallel indicates that this test is safe for parallel execution.
 	// This is true since this is the only test in this file.
-	t.Parallel()
-
-	client, network := interchaintest.DockerSetup(t)
-
-	rep := testreporter.NewNopReporter()
-	eRep := rep.RelayerExecReporter(t)
+	t := s.T()
+	// t.Parallel()
 
 	ctx := context.Background()
 
-	// Get both chains
-	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
+	chainSpecs := []*interchaintest.ChainSpec{
 		// -- WASMD --
 		{
 			ChainConfig: ibc.ChainConfig{
@@ -52,7 +52,6 @@ func TestIcaControllerContract(t *testing.T) {
 				Images: []ibc.DockerImage{
 					{
 						Repository: "cosmwasm/wasmd", // FOR LOCAL IMAGE USE: Docker Image Name
-						// unfortunately, the latest wasmd doesn't work with interchaintest at the moment
 						Version: "v0.40.2", // FOR LOCAL IMAGE USE: Docker Image Tag
 					},
 				},
@@ -90,93 +89,24 @@ func TestIcaControllerContract(t *testing.T) {
 				UsingNewGenesisCommand: true,
 			},
 		},
-	})
+	}
 
-	chains, err := cf.Chains(t.Name())
-	require.NoError(t, err)
+	// This starts the chains, relayer, creates the user accounts, and creates the ibc clients and connections.
+	s.SetupSuite(ctx, chainSpecs)
 
-	wasmd, simd := chains[0].(*cosmos.CosmosChain), chains[1].(*cosmos.CosmosChain)
-
-	// Get a relayer instance
-	customRelayer := relayer.CustomDockerImage("damiannolan/rly", "", "100:1000")
-
-	relayer := interchaintest.NewBuiltinRelayerFactory(
-		ibc.CosmosRly,
-		zaptest.NewLogger(t),
-		relayer.RelayerOptionExtraStartFlags{Flags: []string{"-p", "events", "-b", "100"}},
-		customRelayer,
-	).Build(t, client, network)
-
-	// Build the network; spin up the chains and configure the relayer
-	const (
-		pathName    = "wasmd-simd"
-		relayerName = "relayer"
-	)
-
-	ic := interchaintest.NewInterchain().
-		AddChain(wasmd).
-		AddChain(simd).
-		AddRelayer(relayer, relayerName).
-		AddLink(interchaintest.InterchainLink{
-			Chain1:  wasmd,
-			Chain2:  simd,
-			Relayer: relayer,
-			Path:    pathName,
-		})
-
-	require.NoError(t, ic.Build(ctx, eRep, interchaintest.InterchainBuildOptions{
-		TestName:         t.Name(),
-		Client:           client,
-		NetworkID:        network,
-		SkipPathCreation: true,
-	}))
-
-	// Fund a user account on wasmd and simd
-	const userFunds = int64(10_000_000_000)
-	users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, wasmd, simd)
-	wasmdUser := users[0]
-	simdUser := users[1]
-
-	// Generate a new IBC path
-	err = relayer.GeneratePath(ctx, eRep, wasmd.Config().ChainID, simd.Config().ChainID, pathName)
-	require.NoError(t, err)
-
-	// Create new clients
-	err = relayer.CreateClients(ctx, eRep, pathName, ibc.CreateClientOptions{TrustingPeriod: "330h"})
-	require.NoError(t, err)
-
-	err = testutil.WaitForBlocks(ctx, 2, wasmd, simd)
-	require.NoError(t, err)
-
-	// Create a new connection
-	err = relayer.CreateConnections(ctx, eRep, pathName)
-	require.NoError(t, err)
-
-	err = testutil.WaitForBlocks(ctx, 2, wasmd, simd)
-	require.NoError(t, err)
-
-	// Query for the newly created connection in wasmd
-	connections, err := relayer.GetConnections(ctx, eRep, wasmd.Config().ChainID)
-	require.NoError(t, err)
-	require.Equal(t, 1, len(connections))
-	wasmdConnection := connections[0]
-	require.Equal(t, "connection-0", wasmdConnection.ID)
-
-	// Query for the newly created connection in simd
-	connections, err = relayer.GetConnections(ctx, eRep, simd.Config().ChainID)
-	require.NoError(t, err)
-	// localhost is always a connection in new version of ibc-go
-	require.Equal(t, 2, len(connections))
-	simdConnection := connections[0]
-	require.Equal(t, "connection-0", simdConnection.ID)
+	relayer := s.Relayer
+	wasmd := s.ChainA
+	simd := s.ChainB
+	wasmdUser := s.UserA
+	simdUser := s.UserB
 
 	// Start the relayer and set the cleanup function.
-	err = relayer.StartRelayer(ctx, eRep, pathName)
-	require.NoError(t, err)
+	err := relayer.StartRelayer(ctx, s.ExecRep, s.PathName)
+	s.Require().NoError(err)
 
 	t.Cleanup(
 		func() {
-			err := relayer.StopRelayer(ctx, eRep)
+			err := relayer.StopRelayer(ctx, s.ExecRep)
 			if err != nil {
 				t.Logf("an error occurred while stopping the relayer: %s", err)
 			}
@@ -185,81 +115,92 @@ func TestIcaControllerContract(t *testing.T) {
 
 	// Upload and Instantiate the contract on wasmd:
 	codeId, err := wasmd.StoreContract(ctx, wasmdUser.KeyName(), "../../artifacts/cw_ica_controller.wasm")
-	require.NoError(t, err)
+	s.Require().NoError(err)
 	contractAddr, err := wasmd.InstantiateContract(ctx, wasmdUser.KeyName(), codeId, types.NewInstantiateMsg(nil), true)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
 	contractPort := "wasm." + contractAddr
 
-	// Create Channel between wasmd contract and simd
+	// Test channel handshake between wasmd contract and simd:
+
+	// Query for the newly created connection in wasmd
+	wamsdConnections, err := s.Relayer.GetConnections(ctx, s.ExecRep, s.ChainA.Config().ChainID)
+	s.Require().NoError(err)
+	wasmdConnection := wamsdConnections[0]
+
+	// Query for the newly created connection in simd
+	simdConnections, err := s.Relayer.GetConnections(ctx, s.ExecRep, s.ChainB.Config().ChainID)
+	s.Require().NoError(err)
+	simdConnection := simdConnections[0]
+
 	version := fmt.Sprintf(`{"version":"ics27-1","controller_connection_id":"%s","host_connection_id":"%s","address":"","encoding":"json","tx_type":"sdk_multi_msg"}`, wasmdConnection.ID, simdConnection.ID)
-	err = relayer.CreateChannel(ctx, eRep, pathName, ibc.CreateChannelOptions{
+	err = relayer.CreateChannel(ctx, s.ExecRep, s.PathName, ibc.CreateChannelOptions{
 		SourcePortName: contractPort,
 		DestPortName:   icatypes.HostPortID,
 		Order:          ibc.Ordered,
 		// asking the contract to generate the version by passing an empty string
 		Version: version,
 	})
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
 	// Wait for the channel to get set up
 	err = testutil.WaitForBlocks(ctx, 5, wasmd, simd)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
 	// Test if the handshake was successful
-	wasmdChannels, err := relayer.GetChannels(ctx, eRep, wasmd.Config().ChainID)
-	require.NoError(t, err)
-	require.Equal(t, 1, len(wasmdChannels))
+	wasmdChannels, err := relayer.GetChannels(ctx, s.ExecRep, wasmd.Config().ChainID)
+	s.Require().NoError(err)
+	s.Require().Equal(1, len(wasmdChannels))
 
 	wasmdChannel := wasmdChannels[0]
 	t.Logf("wasmd channel: %s", toJSONString(wasmdChannel))
-	require.Equal(t, contractPort, wasmdChannel.PortID)
-	require.Equal(t, icatypes.HostPortID, wasmdChannel.Counterparty.PortID)
-	require.Equal(t, channeltypes.OPEN.String(), wasmdChannel.State)
+	s.Require().Equal(contractPort, wasmdChannel.PortID)
+	s.Require().Equal(icatypes.HostPortID, wasmdChannel.Counterparty.PortID)
+	s.Require().Equal(channeltypes.OPEN.String(), wasmdChannel.State)
 
-	simdChannels, err := relayer.GetChannels(ctx, eRep, simd.Config().ChainID)
-	require.NoError(t, err)
+	simdChannels, err := relayer.GetChannels(ctx, s.ExecRep, simd.Config().ChainID)
+	s.Require().NoError(err)
 	// I don't know why sometimes an extra channel is created in simd.
 	// this is not related to the localhost connection, and is a failed
 	// clone of the successful channel at index 0. I will log it for now.
-	require.Greater(t, len(simdChannels), 0)
+	s.Require().Greater(len(simdChannels), 0)
 	if len(simdChannels) > 1 {
 		t.Logf("extra simd channel detected: %s", toJSONString(simdChannels[1]))
 	}
 
 	simdChannel := simdChannels[0]
 	t.Logf("simd channel state: %s", toJSONString(simdChannel.State))
-	require.Equal(t, icatypes.HostPortID, simdChannel.PortID)
-	require.Equal(t, contractPort, simdChannel.Counterparty.PortID)
-	require.Equal(t, channeltypes.OPEN.String(), simdChannel.State)
+	s.Require().Equal(icatypes.HostPortID, simdChannel.PortID)
+	s.Require().Equal(contractPort, simdChannel.Counterparty.PortID)
+	s.Require().Equal(channeltypes.OPEN.String(), simdChannel.State)
 
 	// Check contract's channel state
 	queryResp := types.QueryResponse{}
 	err = wasmd.QueryContract(ctx, contractAddr, types.NewGetChannelQueryMsg(), &queryResp)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
 	contractChannelState, err := queryResp.GetChannelState()
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
 	t.Logf("contract's channel store after handshake: %s", toJSONString(contractChannelState))
 
-	require.Equal(t, wasmdChannel.State, contractChannelState.ChannelStatus)
-	require.Equal(t, wasmdChannel.Version, contractChannelState.Channel.Version)
-	require.Equal(t, wasmdChannel.ConnectionHops[0], contractChannelState.Channel.ConnectionID)
-	require.Equal(t, wasmdChannel.ChannelID, contractChannelState.Channel.Endpoint.ChannelID)
-	require.Equal(t, wasmdChannel.PortID, contractChannelState.Channel.Endpoint.PortID)
-	require.Equal(t, wasmdChannel.Counterparty.ChannelID, contractChannelState.Channel.CounterpartyEndpoint.ChannelID)
-	require.Equal(t, wasmdChannel.Counterparty.PortID, contractChannelState.Channel.CounterpartyEndpoint.PortID)
-	require.Equal(t, wasmdChannel.Ordering, contractChannelState.Channel.Order)
+	s.Require().Equal(wasmdChannel.State, contractChannelState.ChannelStatus)
+	s.Require().Equal(wasmdChannel.Version, contractChannelState.Channel.Version)
+	s.Require().Equal(wasmdChannel.ConnectionHops[0], contractChannelState.Channel.ConnectionID)
+	s.Require().Equal(wasmdChannel.ChannelID, contractChannelState.Channel.Endpoint.ChannelID)
+	s.Require().Equal(wasmdChannel.PortID, contractChannelState.Channel.Endpoint.PortID)
+	s.Require().Equal(wasmdChannel.Counterparty.ChannelID, contractChannelState.Channel.CounterpartyEndpoint.ChannelID)
+	s.Require().Equal(wasmdChannel.Counterparty.PortID, contractChannelState.Channel.CounterpartyEndpoint.PortID)
+	s.Require().Equal(wasmdChannel.Ordering, contractChannelState.Channel.Order)
 
 	// Check contract state
 	err = wasmd.QueryContract(ctx, contractAddr, types.NewGetContractStateQueryMsg(), &queryResp)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 	contractState, err := queryResp.GetContractState()
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
-	require.Equal(t, wasmdUser.FormattedAddress(), contractState.Admin)
-	require.Equal(t, wasmdChannel.ChannelID, contractState.IcaInfo.ChannelID)
+	s.Require().Equal(wasmdUser.FormattedAddress(), contractState.Admin)
+	s.Require().Equal(wasmdChannel.ChannelID, contractState.IcaInfo.ChannelID)
 
 	icaAddress := contractState.IcaInfo.IcaAddress
 	t.Logf("ICA address after handshake: %s", icaAddress)
@@ -270,31 +211,32 @@ func TestIcaControllerContract(t *testing.T) {
 		Denom:   simd.Config().Denom,
 		Amount:  1000000000,
 	})
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
+	// wait for 2 blocks for the funds to be received
 	err = testutil.WaitForBlocks(ctx, 2, wasmd, simd)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
 	// Take predefined action on the ICA through the contract:
 	_, err = wasmd.ExecuteContract(ctx, wasmdUser.KeyName(), contractAddr, types.NewSendPredefinedActionMsg(simdUser.FormattedAddress()))
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
-	err = testutil.WaitForBlocks(ctx, 4, wasmd, simd)
-	require.NoError(t, err)
+	err = testutil.WaitForBlocks(ctx, 5, wasmd, simd)
+	s.Require().NoError(err)
 
 	icaBalance, err := simd.GetBalance(ctx, icaAddress, simd.Config().Denom)
-	require.NoError(t, err)
-	require.Equal(t, int64(1000000000-100), icaBalance)
+	s.Require().NoError(err)
+	s.Require().Equal(int64(1000000000-100), icaBalance)
 
 	// Check if contract callbacks were executed:
 	err = wasmd.QueryContract(ctx, contractAddr, types.NewGetCallbackCounterQueryMsg(), &queryResp)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
 	callbackCounter, err := queryResp.GetCallbackCounter()
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
-	require.Equal(t, uint64(1), callbackCounter.Success)
-	require.Equal(t, uint64(0), callbackCounter.Error)
+	s.Require().Equal(uint64(1), callbackCounter.Success)
+	s.Require().Equal(uint64(0), callbackCounter.Error)
 
 	// Send custom ICA messages through the contract:
 	// Let's create a governance proposal on simd and deposit some funds to it.
@@ -304,7 +246,7 @@ func TestIcaControllerContract(t *testing.T) {
 		Description: "tokens for all!",
 	}
 	protoAny, err := codectypes.NewAnyWithValue(&testProposal)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 	proposalMsg := &govtypes.MsgSubmitProposal{
 		Content:        protoAny,
 		InitialDeposit: sdk.NewCoins(sdk.NewCoin(simd.Config().Denom, sdkmath.NewInt(5000))),
@@ -323,26 +265,26 @@ func TestIcaControllerContract(t *testing.T) {
 
 	// Execute them:
 	_, err = wasmd.ExecuteContract(ctx, wasmdUser.KeyName(), contractAddr, customMsg)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
 	err = testutil.WaitForBlocks(ctx, 4, wasmd, simd)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
 	// Check if contract callbacks were executed:
 	err = wasmd.QueryContract(ctx, contractAddr, types.NewGetCallbackCounterQueryMsg(), &queryResp)
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
 	callbackCounter, err = queryResp.GetCallbackCounter()
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
-	require.Equal(t, uint64(2), callbackCounter.Success)
-	require.Equal(t, uint64(0), callbackCounter.Error)
+	s.Require().Equal(uint64(2), callbackCounter.Success)
+	s.Require().Equal(uint64(0), callbackCounter.Error)
 
 	// Check if the proposal was created:
 	proposal, err := simd.QueryProposal(ctx, "1")
-	require.NoError(t, err)
-	require.Equal(t, simd.Config().Denom, proposal.TotalDeposit[0].Denom)
-	require.Equal(t, fmt.Sprint(10000000+5000), proposal.TotalDeposit[0].Amount)
+	s.Require().NoError(err)
+	s.Require().Equal(simd.Config().Denom, proposal.TotalDeposit[0].Denom)
+	s.Require().Equal(fmt.Sprint(10000000+5000), proposal.TotalDeposit[0].Amount)
 	// We do not check title and description of the proposal because this is a legacy proposal.
 }
 
--- e2e/interchaintest/testsuite/suite.go
+++ e2e/interchaintest/testsuite/suite.go
@@ -12,13 +12,12 @@ import (
 
 	dockerclient "github.com/docker/docker/client"
 
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 )
 
-type ContractTestSuite struct {
+type TestSuite struct {
 	suite.Suite
 
 	ChainA *cosmos.CosmosChain
@@ -29,15 +28,17 @@ type ContractTestSuite struct {
 	Relayer ibc.Relayer
 	network string
 	logger *zap.Logger
+	ExecRep *testreporter.RelayerExecReporter
+	PathName string
 }
 
-func (s *ContractTestSuite) SetupSuite(chainSpecs []*interchaintest.ChainSpec) {
+// SetupSuite sets up the chains, relayer, user accounts, clients, and connections
+func (s *TestSuite) SetupSuite(ctx context.Context, chainSpecs []*interchaintest.ChainSpec) {
 	if len(chainSpecs) != 2 {
 		panic("ContractTestSuite requires exactly 2 chain specs")
 	}
 
 	t := s.T()
-	ctx := context.Background()
 
 	s.logger = zaptest.NewLogger(t)
 	s.dockerClient, s.network = interchaintest.DockerSetup(t)
@@ -46,16 +47,11 @@ func (s *ContractTestSuite) SetupSuite(chainSpecs []*interchaintest.ChainSpec) {
 
 	chains, err := cf.Chains(t.Name())
 	s.Require().NoError(err)
-	s.ChainA, s.ChainB = chains[0].(*cosmos.CosmosChain), chains[1].(*cosmos.CosmosChain)
+	s.ChainA = chains[0].(*cosmos.CosmosChain)
+	s.ChainB = chains[1].(*cosmos.CosmosChain)
 
 	// This is currently the only relayer image that works with the main (next) version of ibc-go 
 	customRelayerImage := relayer.CustomDockerImage("damiannolan/rly", "", "100:1000")
-
-	// Fund a user account on ChainA and ChainB
-	const userFunds = int64(10_000_000_000)
-	users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, s.ChainA, s.ChainB)
-	s.UserA = users[0]
-	s.UserB = users[1]
 
 	s.Relayer = interchaintest.NewBuiltinRelayerFactory(
 		ibc.CosmosRly,
@@ -63,14 +59,10 @@ func (s *ContractTestSuite) SetupSuite(chainSpecs []*interchaintest.ChainSpec) {
 		relayer.RelayerOptionExtraStartFlags{Flags: []string{"-p", "events", "-b", "100"}},
 		customRelayerImage,
 	).Build(t, s.dockerClient, s.network)
-}
 
-func (s *ContractTestSuite) SetupClientsAndConnections() {
-	t := s.T()
-	ctx := context.Background()
-	eRep := testreporter.NewNopReporter().RelayerExecReporter(t)
+	s.ExecRep = testreporter.NewNopReporter().RelayerExecReporter(t)
 
-	pathName := s.ChainA.Config().Name + "-" + s.ChainB.Config().Name
+	s.PathName = s.ChainA.Config().Name + "-" + s.ChainB.Config().Name
 
 	ic := interchaintest.NewInterchain().
 		AddChain(s.ChainA).
@@ -80,10 +72,10 @@ func (s *ContractTestSuite) SetupClientsAndConnections() {
 			Chain1:  s.ChainA,
 			Chain2:  s.ChainB,
 			Relayer: s.Relayer,
-			Path:    pathName,
+			Path:    s.PathName,
 		})
 
-	s.Require().NoError(ic.Build(ctx, eRep, interchaintest.InterchainBuildOptions{
+	s.Require().NoError(ic.Build(ctx, s.ExecRep, interchaintest.InterchainBuildOptions{
 		TestName:         t.Name(),
 		Client:           s.dockerClient,
 		NetworkID:        s.network,
@@ -91,36 +83,42 @@ func (s *ContractTestSuite) SetupClientsAndConnections() {
 		SkipPathCreation: true,
 	}))
 
+	// Fund a user account on ChainA and ChainB
+	const userFunds = int64(10_000_000_000)
+	users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, s.ChainA, s.ChainB)
+	s.UserA = users[0]
+	s.UserB = users[1]
+
 	// Generate a new IBC path
-	err := s.Relayer.GeneratePath(ctx, eRep, s.ChainA.Config().ChainID, s.ChainB.Config().ChainID, pathName)
+	err = s.Relayer.GeneratePath(ctx, s.ExecRep, s.ChainA.Config().ChainID, s.ChainB.Config().ChainID, s.PathName)
 	s.Require().NoError(err)
 
 	// Create new clients
-	err = s.Relayer.CreateClients(ctx, eRep, pathName, ibc.CreateClientOptions{TrustingPeriod: "330h"})
+	err = s.Relayer.CreateClients(ctx, s.ExecRep, s.PathName, ibc.CreateClientOptions{TrustingPeriod: "330h"})
 	s.Require().NoError(err)
 
 	err = testutil.WaitForBlocks(ctx, 2, s.ChainA, s.ChainB)
 	s.Require().NoError(err)
 
 	// Create a new connection
-	err = s.Relayer.CreateConnections(ctx, eRep, pathName)
+	err = s.Relayer.CreateConnections(ctx, s.ExecRep, s.PathName)
 	s.Require().NoError(err)
 
 	err = testutil.WaitForBlocks(ctx, 2, s.ChainA, s.ChainB)
 	s.Require().NoError(err)
 
 	// Query for the newly created connection in wasmd
-	connections, err := s.Relayer.GetConnections(ctx, eRep, s.ChainA.Config().ChainID)
+	connections, err := s.Relayer.GetConnections(ctx, s.ExecRep, s.ChainA.Config().ChainID)
 	s.Require().NoError(err)
-	require.Equal(t, 1, len(connections))
+	s.Require().Equal(1, len(connections))
 	wasmdConnection := connections[0]
-	require.Equal(t, "connection-0", wasmdConnection.ID)
+	s.Require().Equal("connection-0", wasmdConnection.ID)
 
 	// Query for the newly created connection in simd
-	connections, err = s.Relayer.GetConnections(ctx, eRep, s.ChainB.Config().ChainID)
+	connections, err = s.Relayer.GetConnections(ctx, s.ExecRep, s.ChainB.Config().ChainID)
 	s.Require().NoError(err)
 	// localhost is always a connection in main (next) version of ibc-go
-	require.Equal(t, 2, len(connections))
+	s.Require().Equal(2, len(connections))
 	simdConnection := connections[0]
-	require.Equal(t, "connection-0", simdConnection.ID)
+	s.Require().Equal("connection-0", simdConnection.ID)
 }
