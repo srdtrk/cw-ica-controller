dae09752909cde45c2042acc4bcc177b8b2ecf08
srdtrk <srdtrk@hotmail.com>
2024-01-22 22:25:47 +0300

    refactor: removed more code

--- e2e/interchaintest/types/cosmwasm.go
+++ e2e/interchaintest/types/cosmwasm.go
@@ -8,22 +8,6 @@ type QueryResponse[T any] struct {
 	Response json.RawMessage `json:"data"`
 }
 
-// CwIbcEndpoint is the endpoint of a channel defined in CosmWasm
-type CwIbcEndpoint struct {
-	PortID    string `json:"port_id"`
-	ChannelID string `json:"channel_id"`
-}
-
-// CwIbcChannel is the channel defined in CosmWasm
-type CwIbcChannel struct {
-	Endpoint             CwIbcEndpoint `json:"endpoint"`
-	CounterpartyEndpoint CwIbcEndpoint `json:"counterparty_endpoint"`
-	// Order is either "ORDER_UNORDERED" or "ORDER_ORDERED"
-	Order        string `json:"order"`
-	Version      string `json:"version"`
-	ConnectionID string `json:"connection_id"`
-}
-
 // GetResp unmarshals the response to a T
 func (qr QueryResponse[T]) GetResp() (T, error) {
 	var resp T
