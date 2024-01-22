9f429e7da932c527d77c1285846f86e3a25ec082
srdtrk <srdtrk@hotmail.com>
2024-01-23 00:45:15 +0300

    deps(testing/callback_counter): changed contract name

--- testing/contracts/callback-counter/src/bin/schema.rs
+++ testing/contracts/callback-counter/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use cw_ica_owner::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use callback_counter::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
