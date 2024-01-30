7f883a7afbed8a22fc25f4bd089c0ff42e18297b
srdtrk <srdtrk@hotmail.com>
2024-01-30 23:46:16 +0100

    imp: added search bar

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -49,7 +49,20 @@ const config: Config = {
   ],
 
   themes: [
-    'docusaurus-theme-github-codeblock'
+    'docusaurus-theme-github-codeblock',
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      }),
+    ],
   ],
 
   themeConfig: {
